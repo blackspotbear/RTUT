5ab7285d932e3ae0c9f2ab87d2b012777d394b0c
blackspotbear <junichi.kamai@gmail.com>
2018-04-21 13:33:21 +0900

    Follow chapter 3

--- Assets/Scripts/RayTracer.cs
+++ Assets/Scripts/RayTracer.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+class Ray {
+    public Ray() { }
+    public Ray(Vector3 a, Vector3 b) { A = a; B = b; }
+
+    public Vector3 origin() { return A; }
+    public Vector3 direction() { return B; }
+    public Vector3 point_at_parameter(float t) { return A + B * t; }
+
+    public Vector3 A;
+    public Vector3 B;
+}
 
 public class RayTracer : MonoBehaviour {
 
     public RawImage m_image;
 
-    // Use this for initialization
     void Start() {
         var texture = rayTrace(new Texture2D(640, 480));
         texture.Apply();
@@ -16,18 +26,32 @@ public class RayTracer : MonoBehaviour {
         Debug.Log("Hello " + this.m_image.mainTexture.width + "," + this.m_image.mainTexture.height);
     }
 
-    // Update is called once per frame
     void Update() {
+        
+    }
 
+    static private Color color(Ray ray) {
+        var unit_direction = ray.direction().normalized;
+        var t = 0.5f * (unit_direction.y + 1.0f);
+        return (1.0f - t) * (new Color(1, 1, 1)) + t * (new Color(0.5f, 0.7f, 1.0f));
     }
 
     static private Texture2D rayTrace(Texture2D texture) {
-        for (var y = 4; y < texture.height - 4; y++) {
-            for (var x = 4; x < texture.width - 4; x++) {
-                var color = x < texture.width / 3 ? Color.red : x < texture.width / 3 * 2 ? Color.blue : Color.green;
-                texture.SetPixel(x, y, color);
+        var lower_left_corner = new Vector3(-2, -1, -1);
+        var horizontal = new Vector3(4, 0, 0);
+        var vertical = new Vector3(0, 2, 0);
+        var origin = new Vector3(0, 0, 0);
+
+        for (var j = texture.height - 1; j >= 0; j--) {
+            for (var i = 0; i < texture.width; i++) {
+                var u = (float)i / texture.width;
+                var v = (float)j / texture.height;
+                var ray = new Ray(origin, lower_left_corner + horizontal * u + vertical * v);
+                var col = color(ray);
+                texture.SetPixel(i, j, col );
             }
         }
+
         return texture;
     }
 }
