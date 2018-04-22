aeb0a1b4a0464fb3b66f51ea4babce21b56336b4
blackspotbear <junichi.kamai@gmail.com>
2018-04-22 16:07:26 +0900

    Follow chapter 8

--- Assets/Scripts/RayTracer.cs
+++ Assets/Scripts/RayTracer.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+static class Util {
+    static public Vector3 random_in_unit_sphere() {
+        var p = new Vector3();
+        do {
+            p = 2.0f * new Vector3(Random.value, Random.value, Random.value) - new Vector3(1, 1, 1);
+        } while (p.magnitude > 1.0f);
+        return p;
+    }
+
+    static public Vector3 reflect(Vector3 v, Vector3 n) {
+        return v - 2 * Vector3.Dot(v, n) * n;
+    }
+}
+
 class Ray {
     public Ray() { }
     public Ray(Vector3 a, Vector3 b) { A = a; B = b; }
@@ -15,10 +29,45 @@ class Ray {
     public Vector3 B;
 }
 
+abstract class material {
+    public abstract bool scatter(Ray ray_in, hit_record rec, out Color attenuation, out Ray scattered);
+}
+
+class lambertian : material {
+    public lambertian(Color a) {
+        albedo = a;
+    }
+    public override bool scatter(Ray ray_in, hit_record rec, out Color attenuation, out Ray scattered) {
+        var target = rec.p + rec.normal + Util.random_in_unit_sphere();
+        scattered = new Ray(rec.p, target - rec.p);
+        attenuation = albedo;
+        return true;
+    }
+
+    Color albedo;
+}
+
+class metal : material {
+    public metal(Color a, float f) {
+        albedo = a;
+        if (f < 1) fuzz = f; else fuzz = 1;
+    }
+    public override bool scatter(Ray ray_in, hit_record rec, out Color attenuation, out Ray scattered) {
+        var reflected = Util.reflect(ray_in.direction().normalized, rec.normal);
+        scattered = new Ray(rec.p, reflected + fuzz * Util.random_in_unit_sphere());
+        attenuation = albedo;
+        return Vector3.Dot(scattered.direction(), rec.normal) > 0;
+    }
+
+    Color albedo;
+    float fuzz;
+}
+
 struct hit_record {
     public float t;
     public Vector3 p;
     public Vector3 normal;
+    public material mat;
 }
 
 abstract class hitable {
@@ -32,6 +81,7 @@ class hitable_list: List<hitable> {
         rec.normal = new Vector3();
         rec.p = new Vector3();
         rec.t = 0;
+        rec.mat = null;
 
         var hit_anything = false;
         var closest_so_far = t_max;
@@ -50,9 +100,10 @@ class hitable_list: List<hitable> {
 class sphere : hitable {
     public Vector3 center;
     public float radius;
+    public material material;
 
     public sphere() {}
-    public sphere(Vector3 cen, float r) { center = cen; radius = r; }
+    public sphere(Vector3 cen, float r, material mat) { center = cen; radius = r; material = mat; }
 
     public override bool hit(Ray r, float t_min, float t_max, out hit_record rec) {
         var oc = r.origin() - center;
@@ -66,6 +117,7 @@ class sphere : hitable {
                 rec.t = temp;
                 rec.p = r.point_at_parameter(rec.t);
                 rec.normal = (rec.p - center) / radius;
+                rec.mat = material;
                 return true;
             }
             temp = (-b + Mathf.Sqrt(b * b - a * c)) / a;
@@ -73,6 +125,7 @@ class sphere : hitable {
                 rec.t = temp;
                 rec.p = r.point_at_parameter(rec.t);
                 rec.normal = (rec.p - center) / radius;
+                rec.mat = material;
                 return true;
             }
         }
@@ -80,6 +133,7 @@ class sphere : hitable {
         rec.normal = new Vector3();
         rec.p = new Vector3();
         rec.t = 0;
+        rec.mat = null;
 
         return false;
     }
@@ -116,19 +170,16 @@ public class RayTracer : MonoBehaviour {
         
     }
 
-    static private Vector3 random_in_unit_sphere() {
-        var p = new Vector3();
-        do {
-            p = 2.0f * new Vector3(Random.value, Random.value, Random.value) - new Vector3(1, 1, 1);
-        } while (p.magnitude > 1.0f);
-        return p;
-    }
-
-    static private Color color(Ray r, hitable_list world) {
+    static private Color color(Ray r, hitable_list world, int depth) {
         hit_record rec;
         if (world.hit(r, 0.001f, float.MaxValue, out rec)) {
-            var tangent = rec.p + rec.normal + random_in_unit_sphere();
-            return 0.5f * color(new Ray(rec.p, tangent - rec.p), world);
+            Ray scatteded;
+            Color attenuation;
+            if (depth < 50 && rec.mat.scatter(r, rec, out attenuation, out scatteded)) {
+                return attenuation * color(scatteded, world, depth + 1);
+            } else {
+                return Color.black;
+            }
         } else {
             var unit_direction = r.direction().normalized;
             var t = 0.5f * (unit_direction.y + 1.0f);
@@ -140,8 +191,10 @@ public class RayTracer : MonoBehaviour {
         const int ns = 100;
         var lower_left_corner = new Vector3(-2, -1, -1);
         var world = new hitable_list {
-            new sphere(new Vector3(0, 0, -1), 0.5f),
-            new sphere(new Vector3(0, -100.5f, -1), 100)
+            new sphere(new Vector3(0, 0, -1), 0.5f, new lambertian(new Color(0.8f, 0.3f, 0.3f))),
+            new sphere(new Vector3(0, -100.5f, -1), 100, new lambertian(new Color(0.8f, 0.8f, 0.0f))),
+            new sphere(new Vector3(1, 0, -1), 0.5f, new metal(new Color(0.8f, 0.6f, 0.2f), 1.0f)),
+            new sphere(new Vector3(-1, 0, -1), 0.5f, new metal(new Color(0.8f, 0.8f, 0.8f), 0.3f))
         };
         var cam = new camera();
         for (var j = texture.height - 1; j >= 0; j--) {
@@ -151,7 +204,7 @@ public class RayTracer : MonoBehaviour {
                     var u = (i + Random.Range(0f, 1f - float.Epsilon)) / texture.width;
                     var v = (j + Random.Range(0f, 1f - float.Epsilon)) / texture.height;
                     var r = cam.get_ray(u, v);
-                    col += color(r, world);
+                    col += color(r, world, 0);
                 }
                 col /= ns;
                 col = new Color(Mathf.Sqrt(col.r), Mathf.Sqrt(col.g), Mathf.Sqrt(col.b));
